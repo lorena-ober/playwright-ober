4c720217abfb9cea04eb5b9b6c8ce523df647709
Lorena Ober <lober@ffos.hr>
2026-01-14 11:54:52 +0100

    Inicijalna struktura projekta

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
 
 /**
  * Read environment variables from file.
@@ -25,11 +26,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+   baseURL: process.env.BASE_URL, screenshot: 'only-on-failure', video: 'retain-on-failure', trace: 'on-first-retry', 
   },
 
   /* Configure projects for major browsers */
